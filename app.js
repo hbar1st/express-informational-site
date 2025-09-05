190f2d0f7cc5181b31681ce590e38b9aed1be5c6
hbar1st <35541470+hbar1st@users.noreply.github.com>
2025-09-04 22:14:20 -0400

    install template engine ejs

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ app.use((req, res) => {
 // go to this middleware function
 app.use((err, req, res, next) => {
   console.error(err);
-  res.status(500).send(err);
+  // We can now specify the `err.statusCode` that exists in our custom error class and if it does not exist it's probably an internal server error
+  res.status(err.statusCode || 500).send(err.message);
 });
 
 const PORT = process.env.PORT || 3000;
--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,5 +1,4 @@
-// controllers/authorController.js
-
+const CustomNotFoundError = require("../errors/CustomNotFoundError");
 const db = require("../db");
 
 async function getAuthorById(req, res) {
@@ -9,7 +8,8 @@ async function getAuthorById(req, res) {
     const author = await db.getAuthorById(Number(authorId));
 
     if (!author) {
-      res.status(404).send("Author not found");
+        //res.status(404).send("Author not found");
+        throw new CustomNotFoundError("Author not found");
       return;
     }
 
@@ -19,12 +19,11 @@ async function getAuthorById(req, res) {
     res.status(500).send("Internal Server Error");
 
     // or we can call next(error) instead of sending a response here
-    // Using `next(error)` however will only render an error page in the 
+    // Using `next(error)` however will only render an error page in the
     // express' default view and respond with the whole html to the client.
-    // So we will need to create a special type of middleware function if we 
+    // So we will need to create a special type of middleware function if we
     // want a different response and we will get to that... next.
   }
-};
-
+}
 
 module.exports = { getAuthorById };
